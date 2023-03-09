a0031de05798d6c13dee7d30805173bfc971da16
cthulhu38 <clement.garcia26@outlook.fr>
2023-03-09 10:22:13 +0100

    TEST: Test avec each.key OK !

--- 01_container.tf
+++ 01_container.tf
@@ -2,9 +2,11 @@
 resource "docker_container" "foo" {
   for_each = var.containeru
 
-  image    = docker_image.debian.image_id
-  name     = each.key
-  hostname = each.value.hostname
+  image = docker_image.debian.image_id
+  name  = each.key
+
+  # hostname = each.value.hostname
+  hostname = each.key
 
   # Pour "tenir" éveiller mon container je passe les commandes suivantes
   # ... sinon elle se kill toute seule ... 
